bc94c09fcb1fb9b942189f4e95cd2c1ed3f3a553
Melissa Rice <mmrice22@gmail.com>
2020-02-05 08:57:28 -0500

    first commit

--- script.js
+++ script.js
@@ -35,6 +35,9 @@ class AddressBook {
   }
   //   returns the contact with that name
   findContactByName(name) {
+    // .FIND way
+    // return this.contacts.find(contact => contact.name === name);
+    // console.log(melsBook.findContactByName("Jessica"));
     // using a for of to find it
     for (let contact of this.contacts) {
       if (contact.name === name) {
@@ -83,11 +86,11 @@ console.log(melsBook);
 
 // for each loop shuffling through all elements in the array
 // making the print function
-function print(addressBookReference) {
-  addressBookReference.contacts.forEach(contact => {
-    console.log(contact);
-  });
-}
+// function print(addressBookReference) {
+//   addressBookReference.contacts.forEach(contact => {
+//     console.log(contact);
+//   });
+// }
 // using the print function
 // print(melsBook);
 
@@ -109,4 +112,39 @@ function print(addressBookReference) {
 // console.log(melsBook.findContactsByRelation("sister"));
 
 // calling on the contact that has that specific text (so moms phone number is the specific text)
-console.log(melsBook.searchContacts("313.969.8258"));
+// console.log(melsBook.searchContacts("313.969.8258"));
+
+// ****PART TWO OF ADDRESS BOOK LAB*****
+function display() {
+let section = document.querySelector(".contact-container");
+section.innerHTML = "";
+for (let contact of melsBook.contacts) {
+  let card = document.createElement("div");
+  let name = document.createElement("p");
+  name.innerText = `Name: ${contact.name}`;
+  // card.append(name)
+  // section.append(card);
+  let email = document.createElement("p");
+  email.innerText = `Email: ${contact.email}`;
+  // card.append(email)
+  // section.append(card);
+  let phone = document.createElement("p");
+  phone.innerText = `Phone: ${contact.phone}`;
+  // card.append(phone)
+  // section.append(card);
+  let relation = document.createElement("p");
+  relation.innerText = `Relation: ${contact.relation}`;
+  card.append(name, email, phone, relation)
+  section.append(card);
+  }
+}
+display();
+
+
+let form = document.querySelector("form");
+form.addEventListener("submit", () => {
+const formData = new formData (form);
+melsBook.add(formData.get("name"), formData.get("email"), formData.get("phone"), formData.get("relation"))
+display();
+});
+
